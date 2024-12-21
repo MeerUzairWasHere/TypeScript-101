f7c9d12f7ebf45815988919f3727a1fe266ed9b3
Meer Uzair <meer.uxair007@gmail.com>
2024-12-21 17:05:22 +0530

    done

--- 01-typescript-basics/src/01-type-annotations.ts
+++ 01-typescript-basics/src/01-type-annotations.ts
@@ -1,3 +1,4 @@
+// Type Annotations
 // TypeScript Type Annotations allow developers to specify the types of variables, function parameters, return types, and object properties.
 
 let awesomeName: string = "shakeAndBake";
@@ -6,15 +7,15 @@ awesomeName = "something";
 awesomeName = awesomeName.toUpperCase();
 // awesomeName = 20;
 
-console.log(awesomeName);
+console.log(awesomeName); // SOMETHING
 
 let amount: number = 12;
 amount = 12 - 1;
 // amount = 'pants';
-console.log(amount);
+console.log(amount); // 11
 
 let isAwesome: boolean = true;
 isAwesome = false;
 
 // isAwesome = 'shakeAndBake';
-console.log(isAwesome);
+console.log(isAwesome); // FALSE
---
+++ 01-typescript-basics/src/02-type-inference.ts
@@ -0,0 +1,12 @@
+// Type Inference
+
+// The typescript compiler can infer the type of the variable based on the literal value that is assigned when it is defined. Just make sure you are working in the typescript file 😄
+
+let awesome = "awesome";
+console.log(awesome.toUpperCase()); // AWESOME
+
+let count = 123;
+console.log(count.toFixed(2)); // 123.00
+
+let arr = ["a", "b", "c"];
+console.log(arr.join(",")); // "a,b,c"
